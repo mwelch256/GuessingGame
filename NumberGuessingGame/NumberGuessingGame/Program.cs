4cb72675ff92c3db180567e6159be8ecba2c8af9
unknown <mwelch1194@gmail.com>
2019-09-12 14:16:25 -0500

    GuessGame

--- NumberGuessingGame/NumberGuessingGame/Program.cs
+++ NumberGuessingGame/NumberGuessingGame/Program.cs
@@ -4,9 +4,83 @@ namespace NumberGuessingGame
 {
     class Program
     {
-        static void Main(string[] args)
+
+
+
+        static int GetIntegerFromUser(string question)
+
         {
-            Console.WriteLine("Hello World!");
+
+            int integerFromUser;
+            bool success;
+
+
+            do
+
+            {
+
+                Console.WriteLine(question);
+
+                string userResponse = Console.ReadLine();
+
+                success = int.TryParse(userResponse, out integerFromUser);
+
+            } while (success == false);
+
+
+
+            return integerFromUser;
         }
-    }
+
+
+
+        static void Main(string[] args)
+            {
+                Console.WriteLine("Let's play a guessing game! The higher your score, the worse you did!");
+                Console.WriteLine("Start off by picking a value to pick from");
+
+                int max = GetIntegerFromUser("What max range would you like?");
+
+                Random rnd = new Random();
+                int secretNumber = rnd.Next(1, max + 1);
+
+                int guessNum = 0;
+                do
+                {
+                    guessNum = GetIntegerFromUser("Guess a Number Mate");
+                    
+                    if (guessNum < secretNumber)
+                    {
+                        Console.WriteLine("This number that i am thinking is higher mate, Try Again!");
+                        continue;
+                    }
+                    else if (guessNum > secretNumber)
+                    {
+                        Console.WriteLine("This number that i am thinking is lower mate, Try Again!");
+                        continue;
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Correct Bro You Have Special Powers!");
+                    }
+
+
+
+
+
+
+
+
+
+
+
+                } while (guessNum != secretNumber);
+            }
+        }
 }
+
+
+
+
+
